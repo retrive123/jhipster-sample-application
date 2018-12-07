c0df0d00edfb276536c7cd8b06150b20dfa2295a
retrive123 <singlaashutosh>
2018-12-07 17:42:46 +0530

    entity created

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,10 +1,14 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { JhipsterSampleApplicationSecretKeyModule } from './secret-key/secret-key.module';
+import { JhipsterSampleApplicationProductModule } from './product/product.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
+        JhipsterSampleApplicationSecretKeyModule,
+        JhipsterSampleApplicationProductModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
